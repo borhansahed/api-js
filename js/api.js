248c7697096e2c2944bb95e679599a41a05576e9
wahi90 <mdshahed1098@gmail.com>
2022-02-27 22:26:25 +0600

    practice api

---
+++ js/api.js
@@ -0,0 +1,76 @@
+// function loadData(){
+//     fetch('https://www.thesportsdb.com/api/v1/json/2/all_sports.php')
+//     .then(res => res.json())
+//     .then(data => loaduser(data))
+  
+// };
+// function loaduser(tata){
+//     fetch('https://www.thesportsdb.com/api/v1/json/2/all_sports.php/sports')
+//     .then(res => res.json())
+//     .then (tata => displayUser(tata.sports))
+// }
+// function displayUser(data){
+//    const ul = document.getElementById('list-items');
+//  for(const user of data){
+//     console.log(user)
+//    const li = document.createElement('li');
+//    li.innerHTML = `
+                 
+     
+//                     <h4>${user.strSport}</h4>
+//                     <img src="${user.strSportIconGreen}" alt="">`;
+//    ul.appendChild(li);
+    
+//  }
+// }
+// const loadQuotes = () => {
+//   const url = 'https://api.kanye.rest/';
+//   fetch(url)
+//   .then(res => res.json())
+//   .then(data => showQuotes(data))
+// }
+// const showQuotes = quotes => {
+//  const ul = document.getElementById('list-items');
+//  const quotesInput =document.createElement('blockquote')
+//  quotesInput.classList.add('quotes')
+//  quotesInput.innerText = quotes.quote;
+//  ul.appendChild(quotesInput)
+ 
+// }
+const searchFood = () => {
+  const searchInput = document.getElementById('search-input');
+  const searchText = searchInput.value;
+  console.log(searchText);
+  searchInput.value = '';
+  const url =`https://www.themealdb.com/api/json/v1/1/search.php?s=${searchText}`
+  fetch(url)
+  .then(res => res.json())
+  .then(data => displayResult(data.meals))
+}
+
+const displayResult = meals => {
+  const cardDiv = document.getElementById('card-result');
+   cardDiv.innerText= "";
+  for(const items of meals){
+    const newDiv = document.createElement('div');
+  
+    newDiv.innerHTML=`
+    <div class="col w-75 h-75">
+      <div class="card ">
+     <img  src="${items.strMealThumb}" class="items card-img-top w-100 h-75" alt="...">
+     <div class="card-body ">
+      <h5 class="card-title">${items.strMeal}</h5>
+      <p class="card-text">${items.strInstructions.slice(0 ,190)}</p>
+      <p <a href="${items.strInstructions}"></a>>Watch Now</p>
+    </div> 
+      
+     
+    
+    </div>
+    `
+    
+    cardDiv.appendChild(newDiv);
+    
+  }
+}
+
